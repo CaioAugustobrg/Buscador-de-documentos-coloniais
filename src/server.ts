f7133aa9c364282d7f1dcc7d04d008c40b31074d
Caio Braga <caioaugustobrg@gmail.com>
2023-11-06 01:32:48 -0300

    :sparkles: feat: cors etc

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,38 @@
 import express from 'express'
+import router from './routes/index'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 const app = express()
-const port = 3000
+app.use(express.json())
+app.use(bodyParser.json())
+const port = process.env.PORT ?? 3030
+const host = process.env.HOST ?? '127.0.0.1'
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173'
+  })
+)
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'http://127.0.0.1:5173'
+  )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,PUT,PATCH,POST,DELETE,OPTIONS'
+  )
+  next()
+})
 app.get('/', (request, response) => {
   response.type('text/plain')
   response.send('Server is running')
 })
+app.use(router)
 app.listen(port, () => {
-  console.log(`Server is running on http://127.0.0.1:${port}; press ctrl + c to terminate`)
+  console.log(`Server is running on http://${host}:${port}; press ctrl + c to terminate`)
 })
