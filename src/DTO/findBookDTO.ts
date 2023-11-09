7f8c260eb73f5f93b3c4ca4a232e3a1621e04883
Caio Braga <caioaugustobrg@gmail.com>
2023-11-09 01:40:47 -0300

    :sparkles: feat: find book dto

---
+++ src/DTO/findBookDTO.ts
@@ -0,0 +1,21 @@
+export interface BookDTO {
+  AnoF?: string
+  AnoI?: string
+  anoPub?: string
+  autores?: string
+  capitania?: string
+  datas?: string
+  fonte?: string
+  id_X?: string
+  link?: string
+  lugares?: string
+  nomes?: string
+  periodoA?: string
+  ref?: string
+  store_id?: string
+  temaPercent?: string
+  temas?: string
+  tematicas?: string
+  titulo?: string
+
+}
