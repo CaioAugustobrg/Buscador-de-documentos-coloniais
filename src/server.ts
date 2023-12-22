16937c46213fcc994c835ffa9605fc2de0ca653d
Caio Braga <caioaugustobrg@gmail.com>
2023-12-22 19:52:28 -0300

    :sparkles: feat: heroku again

--- src/server.ts
+++ src/server.ts
@@ -10,24 +10,11 @@ const host = process.env.HOST ?? '127.0.0.1'
 app.use(
   cors({
     credentials: true,
-    origin: 'http://127.0.0.1:5173'
+    // origin: '*',
+    origin: 'https://oxossi.vercel.app/',
+    allowedHeaders: ['Content-Type', 'Authorization']
   })
 )
-app.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'http://127.0.0.1:5173'
-  )
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET,PUT,PATCH,POST,DELETE,OPTIONS'
-  )
-  next()
-})
 app.get('/', (request, response) => {
   response.type('text/plain')
   response.send('Server is running')
