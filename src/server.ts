e714174efc5befbdf2061a21ecd02c514a36f5c1
Caio Braga <caioaugustobrg@gmail.com>
2023-12-22 00:15:53 -0300

    heroku config

--- src/server.ts
+++ src/server.ts
@@ -8,31 +8,18 @@ app.use(bodyParser.json())
 const port = process.env.PORT ?? 3030
 const host = process.env.HOST ?? '127.0.0.1'
 app.use(
-  cors({
-    credentials: true,
-    origin: 'http://127.0.0.1:5173'
-  })
-)
-app.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'http://127.0.0.1:5173'
-  )
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET,PUT,PATCH,POST,DELETE,OPTIONS'
-  )
-  next()
+cors({
+credentials: true,
+//origin: '*',
+origin: 'https://oxossi.vercel.app',
+allowedHeaders: ['Content-Type', 'Authorization']
 })
+)
 app.get('/', (request, response) => {
-  response.type('text/plain')
-  response.send('Server is running')
+response.type('text/plain')
+response.send('Server is running')
 })
 app.use(router)
 app.listen(port, () => {
-  console.log(`Server is running on http://${host}:${port}; press ctrl + c to terminate`)
+console.log(`Server is running on http://${host}:${port}; press ctrl + c to terminate`)
 })
