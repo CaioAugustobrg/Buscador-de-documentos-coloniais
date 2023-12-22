2425e4b9d30f9261b8b5345aa80e32dff11fc5cf
Caio Braga <caioaugustobrg@gmail.com>
2023-12-22 00:29:10 -0300

    heroku config

--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,10 @@ app.use(express.json())
 app.use(bodyParser.json())
 const port = process.env.PORT ?? 3030
 const host = process.env.HOST ?? '127.0.0.1'
-app.use(
-cors({
-credentials: true,
-//origin: '*',
-origin: 'https://oxossi.vercel.app',
-allowedHeaders: ['Content-Type', 'Authorization']
-})
-)
+app.use(cors({
+  origin: 'https://oxossi.vercel.app',
+  credentials: true
+}));
 app.get('/', (request, response) => {
 response.type('text/plain')
 response.send('Server is running')
