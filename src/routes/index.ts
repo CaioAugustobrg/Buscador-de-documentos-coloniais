c3c7d678ed4b0facd6055ce3d5ca15a3482520f8
Caio Braga <caioaugustobrg@gmail.com>
2024-01-23 21:04:12 -0300

    :sparkles: feat: deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import express from 'express'
-// import { SearchBookController } from '../controller/searchBookController'
 import { searchBookController } from '../factory/searchBook'
-// import search from '../controllers/search'
 const router = express.Router()
-router.post('/', async (request, response) => {
+router.post('/search', async (request, response) => {
   return await searchBookController.handle(request, response)
 })
 export default router
--- src/useCase/searchBook.ts
+++ src/useCase/searchBook.ts
@@ -1,51 +1,15 @@
-// import { MissingParamError } from '../errors/missingParamError'
 import { type BookDTO } from '../DTO/findBookDTO'
-// import { Book } from '../entities/book'
 import { type JsonBookRepository } from '../repositories/jsonBookRepository'
 import { Book } from '../entities/book'
 import ApiError from '../utils/logs/apiError'
 
-// interface TypedRequest<T> extends HttpRequest {
-//   body: T
-// }
 export class SearchBookUseCase {
   constructor (
     private readonly JsonBookRepository: JsonBookRepository
   ) { }
 
   async handle (httpRequest: BookDTO): Promise<Book | null> {
-    console.log('usecase', httpRequest)
-    // let gotBodyProperty
-    // for (const bodyPropeties in HttpRequest.body) {
-    //   if (bodyPropeties !== '') {
-    //     gotBodyProperty = +1
-    //   }
-    // }
-    // if (gotBodyProperty === 0) {
-    //   return badRequest(new MissingParamError('At least one field'))
-    // }
-    // const {
-    //   AnoF,
-    //   AnoI,
-    //   anoPub,
-    //   autores,
-    //   capitania,
-    //   datas,
-    //   fonte,
-    //   id_X,
-    //   link,
-    //   lugares,
-    //   nomes,
-    //   periodoA,
-    //   ref,
-    //   store_id,
-    //   temaPercent,
-    //   temas,
-    //   tematicas,
-    //   titulo
-    // } = httpRequest.body
     const book = new Book(httpRequest)
-    console.log('usecase', book)
     const findBook = await this.JsonBookRepository.findBook(book)
     if (findBook === null) {
       throw new ApiError({
